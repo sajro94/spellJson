bf1066d46871d734be1620f405e77119ffe300ac
sajro94 <sajro94@gmail.com>
2018-07-09 19:21:35 +0200

    Add files via upload

--- Cold One.js
+++ Cold One.js
@@ -1,55 +1,53 @@
 {
-	"source": "MM",
-	"page": 0,
-	"name": "Cold One",
-	"size": "L",
-	"type": "beast",
-	"alignment": [
-		"U"
-	],
-	"ac": "14 AC  ( Natural Armor[2] )",
-	"hp": {
-		"average": 51,
-		"formula": "6d10+18"
-	},
-	"speed": {
-		"walk": 60
-	},
-	"str": 21,
-	"dex": 15,
-	"con": 17,
-	"int": 3,
-	"wis": 13,
-	"cha": 7,
-	"save": {
-		"con": "+5",
-		"wis": "+3"
-	},
-	"skill": {
-		"athletics": "+7"
-	},
-	"resist": [
-		"cold",
-		"necrotic"
-	],
-	"senses": "",
-	"passive": null,
-	"languages": "\u2014",
-	"cr": "1",
-	"trait": [
-		{
+	"monster": [{
+		"source": "MM",
+		"page": 0,
+		"name": "Cold One",
+		"size": "L",
+		"type": "beast",
+		"alignment": [
+			"U"
+		],
+		"ac": "14 AC  ( Natural Armor[2] )",
+		"hp": {
+			"average": 51,
+			"formula": "6d10+18"
+		},
+		"speed": {
+			"walk": 60
+		},
+		"str": 21,
+		"dex": 15,
+		"con": 17,
+		"int": 3,
+		"wis": 13,
+		"cha": 7,
+		"save": {
+			"con": "+5",
+			"wis": "+3"
+		},
+		"skill": {
+			"athletics": "+7"
+		},
+		"resist": [
+			"cold",
+			"necrotic"
+		],
+		"senses": "",
+		"passive": null,
+		"languages": "\u2014",
+		"cr": "1",
+		"trait": [{
 			"name": "Pack Tactics",
 			"entries": [
 				"The Cold One has advantage on an attack roll against a creature if at least one of the Cold One's allies is within 5 feet of the creature and the ally isn't incapacitated."
 			]
-		}
-	],
-	"action": [
-		{
+		}],
+		"action": [{
 			"name": "Bite",
 			"entries": [
 				"Melee Attack: {@hit +5} to hit., reach 5 ft., one target. *Hit:* 14 ({@dice 2d8+5}) piercing damage."
 			]
-		}
-	]
+		}]
+	}]
 }
